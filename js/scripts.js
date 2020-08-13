a468339852d2c1b69c544ebfa5cebac1e65ff50a
[brycen] <[brycenbartolome@gmail.com]>
2020-08-13 10:33:44 -0700

    add functions to scripts.js
    co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,26 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     const voterInput = $("input#voter").val();
+    const voterAge = parseInt($("input#age").val());
+    //const dob = $("#born").val();
+    //console.log(dob);
+    //const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10)
+    //console.log(voterAge);
 
 
+    $(".hide").hide();
     $(".voter").text(voterInput);
     $(".younger").text(voterInput);
+
+    if (voterAge >= 18) {
+      $('#vote').show();
+    } else {
+      $('#too-young').show();
+    }
+
+
 
     event.preventDefault();
   });
 });
+
